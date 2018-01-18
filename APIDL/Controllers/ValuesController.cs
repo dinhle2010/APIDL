5edd20bcf51e07e5721acacc928615ca54cf662e
ERP_LMDinh\ConcungPC <dinh.le@concung.com>
2018-01-18 14:14:45 +0700

    no message

---
+++ APIDL/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIDL.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get(string EndPoint)
+        {
+            try
+            {
+                HttpClient _client = new HttpClient();
+                using (var result = _client.PostAsync(EndPoint, null))
+                {
+                    return result.Result.Content.ReadAsStringAsync().Result;
+                };
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+        }
+
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
