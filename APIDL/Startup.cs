8dc6294ab0bd6007e388af8c4813acfb12e54c4d
ERP_LMDinh\ConcungPC <dinh.le@concung.com>
2018-01-18 14:48:36 +0700

    no message

--- APIDL/Startup.cs
+++ APIDL/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -33,7 +34,12 @@ namespace APIDL
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
