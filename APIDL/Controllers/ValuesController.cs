1c79de3810b9f019d3141e96b296a93b193d1603
ERP_LMDinh\ConcungPC <dinh.le@concung.com>
2018-01-18 18:34:31 +0700

    no message

--- APIDL/Controllers/ValuesController.cs
+++ APIDL/Controllers/ValuesController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -39,8 +42,24 @@ namespace APIDL.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public string Post(PoloParam objPolo)
         {
+            try
+            {
+                objPolo.init();
+                HttpClient _client = new HttpClient();
+                _client.DefaultRequestHeaders.Add("key", objPolo.Key);
+                _client.DefaultRequestHeaders.Add("Sign", objPolo.Sign);
+
+                using (var result = _client.PostAsync(objPolo.EndPoint, null))
+                {
+                    return result.Result.Content.ReadAsStringAsync().Result;
+                };
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
         }
 
         // PUT api/values/5
@@ -53,6 +72,43 @@ namespace APIDL.Controllers
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+    }
+    public class PoloParam
+    {
+        public string EndPoint { get; set; }
+        public string Key { get; set; }
+        public string Sign { get; set; }
+        Dictionary<string, object> Param { get; set; }
+        public void init()
+        {
+            Sign = PoloHelper.MD5Hash(Sign);
+        }
+    }
+    public static class PoloHelper
+    {
+        //private HttpWebRequest CreateHttpWebRequest(string method, string relativeUrl)
+        //{
+        //    ServicePointManager.Expect100Continue = true;
+        //    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3;
+        //    var request = WebRequest.CreateHttp(BaseUrl + relativeUrl);
+        //    request.Method = method;
+        //    request.UserAgent = "Poloniex API .NET v" + Helper.AssemblyVersionString;
+        //    //request.Timeout = Timeout.Infinite;
+        //    request.Credentials = CredentialCache.DefaultCredentials;
+
+        //    request.Headers[HttpRequestHeader.AcceptEncoding] = "gzip,deflate";
+        //    request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+        //    return request;
+        //}
+        public static string MD5Hash(string input)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var result = md5.ComputeHash(Encoding.ASCII.GetBytes(input));
+                return Encoding.ASCII.GetString(result);
+            }
         }
     }
 }
