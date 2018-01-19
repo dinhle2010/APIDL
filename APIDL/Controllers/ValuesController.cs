cd1cdbc17953344a71ba674ef3f4d6f040646cbb
MINHDINH-PC\Dinh <dinh.leminh88@gmail.com>
2018-01-19 22:34:19 +0700

    no message

--- APIDL/Controllers/ValuesController.cs
+++ APIDL/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -46,22 +47,31 @@ namespace APIDL.Controllers
         {
             try
             {
-                //objPolo.init();
-                HttpClient _client = new HttpClient();
-                _client.DefaultRequestHeaders.Add("Key", objPolo.Key);
-                _client.DefaultRequestHeaders.Add("Sign", objPolo.Sign);
-
-                using (var result = _client.PostAsync(objPolo.EndPoint, null))
+                using (var client = new HttpClient())
                 {
+                    client.BaseAddress = new Uri(objPolo.EndPoint);
+                    StringContent myContent = new StringContent(objPolo.myParam);
+                    myContent.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+                    client.DefaultRequestHeaders.Add("Key", objPolo.Key);
+                    client.DefaultRequestHeaders.Add("Sign", genHMAC(objPolo.myParam,objPolo.Sign));
+                    var result =  client.PostAsync(objPolo.EndPoint, myContent);
                     return result.Result.Content.ReadAsStringAsync().Result;
-                };
+                }
+               
             }
             catch (Exception ex)
             {
                 return ex.Message;
             }
         }
-
+        private string genHMAC(string message,string ApiSecret)
+        {
+            var hmac = new HMACSHA512(Encoding.ASCII.GetBytes(ApiSecret));
+            var messagebyte = Encoding.ASCII.GetBytes(message);
+            var hashmessage = hmac.ComputeHash(messagebyte);
+            var sign = BitConverter.ToString(hashmessage).Replace("-", "").ToLower();
+            return sign;
+        }
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
@@ -78,7 +88,8 @@ namespace APIDL.Controllers
     {
         public string EndPoint { get; set; }
         public string Key { get; set; }
-        public string Sign { get; set; }       
+        public string Sign { get; set; }
+        public string myParam { get; set; }
     }
     public static class PoloHelper
     {
